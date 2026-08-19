b69d01ae145a83779bb65d96157415bdb00ee946
Maruyama-riki <gabrielriki1214@gmail.com>
2026-08-19 17:37:52 +0000

    1

--- script.js
+++ script.js
@@ -16,8 +16,34 @@ document.addEventListener('DOMContentLoaded', () => {
             let pokeImage = document.getElementById("poke-img");
             pokeImage.src = data.sprites.front_default;
 
-            let pokeSprites = document.getElementById("poke-sprite");
-            pokeMoves.src = data.sprites.front_default;
+            document.getElementById("poke-nome").textContent = data.name;
+            document.getElementById("poke-id").textContent = data.id;
+            document.getElementById("poke-tipos").textContent = data.types.map(t => t.type.name).join(", ");
+            document.getElementById("poke-altura").textContent = (data.height / 10) + " m";
+            document.getElementById("poke-peso").textContent = (data.weight / 10) + " kg";
+            document.getElementById("poke-forms").textContent = data.forms.map(f => f.name).join(", ");
+            document.getElementById("poke-abilities").textContent = data.abilities.map(a => a.ability.name).join(", ");
+            document.getElementById("poke-sprites").textContent = data.abilities.map(a => a.ability.name).join(", ");
+            let sprites = [
+    data.sprites.front_default,
+    data.sprites.back_default,
+    data.sprites.front_shiny,
+    data.sprites.back_shiny
+];
+
+let galeria = document.getElementById("poke-galeria"); // uma <div id="poke-galeria"></div> no HTML
+galeria.innerHTML = ""; // limpa antes de adicionar de novo
+
+sprites.forEach(url => {
+    if (url) {
+        let img = document.createElement("img");
+        img.src = url;
+        img.style.width = "80px";
+        galeria.appendChild(img);
+    }
+});
+
+
         } catch (error) {
             console.log("Erro: " + error);
         }
